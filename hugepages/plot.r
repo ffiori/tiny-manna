64ba2d109e4cf22af31eeec3739a108fd8ece0db
Emilio López <elopez93@gmail.com>
2018-07-15 04:03:56 +0300

    Plot comparison graphs

--- hugepages/plot.r
+++ hugepages/plot.r
@@ -1,34 +1,55 @@
 library("ggplot2")
 library("scales")
-x = read.csv(file, header=F)
-res = aggregate(x[,-1], list(x[,1]), mean)
 
-names(res) <- c("slots", "ipc", "missratio", "time")
-res <- transform(res, normtime = slots / time)
+get_data <- function(file) {
+  x = read.csv(file, header=F)
+  res = aggregate(x[,-1], list(x[,1]), mean)
 
-ggplot(res, aes(x = slots, y = normtime)) +
+  names(res) <- c("slots", "ipc", "missratio", "time")
+  res <- transform(res, normtime = slots / time)
+}
+
+fast <- get_data(filefast)
+slow <- get_data(fileslow)
+slow$slots <- NULL
+names(slow) <- c("ipcslow", "missratioslow", "timeslow", "normtimeslow")
+res <- cbind(fast, slow)
+
+ggplot(res, aes(x = slots, y = normtime, colour = "Optimizado")) +
   geom_line() +
   geom_point() +
+  geom_line(aes(y = normtimeslow, colour = "Original")) +
+  geom_point(aes(y = normtimeslow, colour = "Original")) +
   scale_x_log10(labels = comma) +
   labs(y = "Tiempo de ejecución normalizado (slots/s)") +
   labs(x = "Tamaño del problema (slots, log)") +
-  theme(legend.title=element_blank()) + 
-  geom_vline(xintercept=8192) + geom_vline(xintercept=65536) + geom_vline(xintercept=3932160)
+  theme(legend.title=element_blank()) +
+  geom_vline(xintercept=8192) + # L1
+  geom_vline(xintercept=65536) + # L2
+  geom_vline(xintercept=3932160) # L3
 
-ggplot(res, aes(x = slots, y = ipc)) +
+ggplot(res, aes(x = slots, y = ipc, colour = "Optimizado")) +
   geom_line() +
   geom_point() +
+  geom_line(aes(y = ipcslow, colour = "Original")) +
+  geom_point(aes(y = ipcslow, colour = "Original")) +
   scale_x_log10(labels = comma) +
   labs(y = "Instrucciones por ciclo (IPC)") +
   labs(x = "Tamaño del problema (slots, log)") +
   theme(legend.title=element_blank()) +
-  geom_vline(xintercept=8192) + geom_vline(xintercept=65536) + geom_vline(xintercept=3932160)
+  geom_vline(xintercept=8192) + # L1
+  geom_vline(xintercept=65536) + # L2
+  geom_vline(xintercept=3932160) # L3
 
-ggplot(res, aes(x = slots, y = missratio)) +
+ggplot(res, aes(x = slots, y = missratio, colour = "Optimizado")) +
   geom_line() +
   geom_point() +
+  geom_line(aes(y = missratioslow, colour = "Original")) +
+  geom_point(aes(y = missratioslow, colour = "Original")) +
   scale_x_log10(labels = comma) +
   labs(y = "Cache miss ratio (%)") +
   labs(x = "Tamaño del problema (slots, log)") +
   theme(legend.title=element_blank()) +
-  geom_vline(xintercept=8192) + geom_vline(xintercept=65536) + geom_vline(xintercept=3932160)
+  geom_vline(xintercept=8192) + # L1
+  geom_vline(xintercept=65536) + # L2
+  geom_vline(xintercept=3932160) # L3
--- hugepages/process.sh
+++ hugepages/process.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-RESULTFILE="$1"
-TMPOUT=$(mktemp)
 
-grep -E '(seconds|slots|instructions|cache-misses)' "$RESULTFILE" | awk '{if (NR%4==0) printf ",%f", $1; else if (NR%4==1) printf "%d", $8; else printf ",%f", $4; if (NR%4==0) printf "\n"}' > "$TMPOUT"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <optimized.txt> <original.txt>"
+    exit 1
+fi
 
-(printf 'file="%s"\n' "$TMPOUT"; cat plot.r) | R --vanilla >/dev/null
-mv Rplots.pdf "$RESULTFILE.pdf"
+RESULTFILE1="$1"
+RESULTFILE2="$2"
+TMPOUT1=$(mktemp)
+TMPOUT2=$(mktemp)
 
-rm "$TMPOUT"
+grep -E '(seconds|slots|instructions|cache-misses)' "$RESULTFILE1" | awk '{if (NR%4==0) printf ",%f", $1; else if (NR%4==1) printf "%d", $8; else printf ",%f", $4; if (NR%4==0) printf "\n"}' > "$TMPOUT1"
+grep -E '(seconds|slots|instructions|cache-misses)' "$RESULTFILE2" | awk '{if (NR%4==0) printf ",%f", $1; else if (NR%4==1) printf "%d", $8; else printf ",%f", $4; if (NR%4==0) printf "\n"}' > "$TMPOUT2"
+
+(printf 'filefast="%s"\nfileslow="%s"\n' "$TMPOUT1" "$TMPOUT2"; cat plot.r) | R --vanilla >/dev/null
+mv Rplots.pdf "$RESULTFILE1+$RESULTFILE2.pdf"
+
+rm "$TMPOUT1" "$TMPOUT2"
