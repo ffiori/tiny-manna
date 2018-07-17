f6d0fee9d7cffd35a490f549475dafe8a33b4ff1
Fernando Fiori <cp201820@zx81.famaf.unc.edu.ar>
2018-07-17 15:11:51 -0300

    simd works but slowly

--- tp2/tiny_manna.cpp
+++ tp2/tiny_manna.cpp
@@ -177,7 +177,7 @@ unsigned int descargar(Manna_Array __restrict__ h_, Manna_Array __restrict__ dh_
 		//~ }
 	//~ }
 	
-	for (i = N-4; i < N; ++i) {
+	for (i = N-4; i < N; ++i) if(h[i] > 1) {
 		for (int j = 0; j < h[i]; ++j) {
 			int k = (i+2*randbool()-1+N)%N;
 			++dh[k];
@@ -185,41 +185,54 @@ unsigned int descargar(Manna_Array __restrict__ h_, Manna_Array __restrict__ dh_
 		h[i] = 0;
 	}
 	
-	for (i = 0; i < 4; ++i) {
+	for (i = 0; i < 4; ++i) if(h[i] > 1) {
 		for (int j = 0; j < h[i]; ++j) {
 			int k = (i+2*randbool()-1+N)%N;
 			++dh[k];
 		}
 		h[i] = 0;
 	}
-	
+
+	const __m128i zeroes = _mm_set_epi32(0,0,0,0);
+	const __m128i ones = _mm_set_epi32(1,1,1,1);
+
 	for (; i < N-4; i+=4) {
-		__m128i left = _mm_load_si128((__m128i *) &dh[i-1]);
-		__m128i right = _mm_load_si128((__m128i *) &dh[i+1]);
-		__m128i slots = _mm_load_si128((__m128i *) &h[i]);
-		const __m128i zeroes = _mm_set_epi32(0,0,0,0);
-		const __m128i ones = _mm_set_epi32(1,1,1,1);
-		__m128i slots_gt1 = _mm_cmplt_epi32(ones,slots); //slots greater than 1>1
+		__m128i left = zeroes; //_mm_loadu_si128((__m128i *) &dh[i-1]);
+		__m128i right = zeroes; //_mm_loadu_si128((__m128i *) &dh[i+1]);
+		__m128i slots = _mm_loadu_si128((__m128i *) &h[i]);
+		
+		__m128i slots_gt1 = _mm_cmpgt_epi32(slots,ones); //slots greater than 1
 		__m128i active_slots;
-		while(active_slots = slots_gt1 & _mm_cmpgt_epi32(slots,zeroes), active_slots[0] | active_slots[1]){ //chequear
+		while(active_slots = _mm_and_si128(slots_gt1, _mm_cmpgt_epi32(slots,zeroes)), _mm_movemask_epi8(active_slots)){ //chequear
 			__m128i randomright = _mm_set_epi32(randbool(),randbool(),randbool(),randbool()); //TODO optimize
 			//const int r = rand();
 			//__m128i randomright = _mm_blend_epi32(ones,zeroes,r); //con un rand de 0 a (1<<4)-1 alcanza, se puede hacer con el de la STL
 			__m128i randomleft = _mm_xor_si128(randomright, ones);
-
+//~ cout<<"randoms: "<<randomright[0]<<" "<<randomright[1]<<" "<<randomleft[0]<<" " <<randomleft[1]<<endl;
 			__m128i addright = _mm_and_si128(randomright, active_slots);
 			__m128i addleft = _mm_and_si128(randomleft, active_slots);
 
 			left = _mm_add_epi32(left, addleft);
 			right = _mm_add_epi32(right, addright);
 
-			slots = _mm_sub_epi32(slots, active_slots & ones); // slots - (active_slots & ones), le resto 1 a cada slot activo
+			slots = _mm_sub_epi32(slots, _mm_and_si128(active_slots, ones)); // slots - (active_slots & ones), le resto 1 a cada slot activo
 		}
-		
+		//~ cout<<"update l y r: "<<left[0]<<" "<<left[1]<<" "<<right[0]<<" "<<right[1]<<endl;
 		//escribo en dh
-		_mm_store_si128((__m128i *) &dh[i-1],left);
-		_mm_store_si128((__m128i *) &dh[i+1],right);
-		_mm_store_si128((__m128i *) &h[i],slots);
+		__m128i mitad = _mm_srli_si128(right,8); //_mm_set_epi32(0,0,_mm_extract_epi32(right,0),_mm_extract_epi32(right,1));
+		left = _mm_add_epi32(left,mitad);
+		
+		__m128i leftold = _mm_loadu_si128((__m128i *) &dh[i-1]);
+		left = _mm_add_epi32(left,leftold);
+		_mm_storeu_si128((__m128i *) &dh[i-1],left);
+		
+		mitad = _mm_slli_si128(right,8); //_mm_set_epi32(0,0,_mm_extract_epi32(right,2),_mm_extract_epi32(right,3));
+		mitad = _mm_srli_si128(mitad,8);
+		__m128i rightold = _mm_loadu_si128((__m128i *) &dh[i+1]);
+		right = _mm_add_epi32(rightold,mitad);
+		_mm_storeu_si128((__m128i *) &dh[i+1],right);
+		
+		_mm_storeu_si128((__m128i *) &h[i],slots);
 	}
 
 //h[0] = 0x7777; //DUMMY
