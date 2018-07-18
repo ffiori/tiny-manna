6254772a355f47c2ce8ef124f7795be7b1eea68d
Fefo <fefo_f93@hotmail.com>
2018-07-18 17:54:49 -0300

    simd256, needs fix

--- tp2/tiny_manna.cpp
+++ tp2/tiny_manna.cpp
@@ -27,6 +27,7 @@ Notar que si la densidad de granitos, [Suma_i h[i]/N] es muy baja, la actividad 
 #include <x86intrin.h> //SIMD
 
 #define printear(leftold) _mm_extract_epi32(leftold,0)<<" "<<_mm_extract_epi32(leftold,1)<<" "<<_mm_extract_epi32(leftold,2)<<" "<<_mm_extract_epi32(leftold,3)
+#define printear256(leftold) _mm256_extract_epi32(leftold,7)<<" "<<_mm256_extract_epi32(leftold,6)<<" "<<_mm256_extract_epi32(leftold,5)<<" "<<_mm256_extract_epi32(leftold,4)<<" "<<_mm256_extract_epi32(leftold,3)<<" "<<_mm256_extract_epi32(leftold,2)<<" "<<_mm256_extract_epi32(leftold,1)<<" "<<_mm256_extract_epi32(leftold,0)
 
 // number of sites
 //#define N (1024 / 4) //2MB data
@@ -46,6 +47,10 @@ typedef int * Manna_Array;
 
 static default_random_engine generator;
 
+const __m256i zeroes = _mm256_set_epi32(0,0,0,0,0,0,0,0);
+const __m128i zeroes128 = _mm_set_epi32(0,0,0,0);
+const __m256i ones = _mm256_set_epi32(1,1,1,1,1,1,1,1);
+
 #ifndef SEED
 #define SEED 0
 #endif
@@ -59,6 +64,14 @@ void randinit() {
 static inline bool randbool() {
         uniform_int_distribution<int> distribution(0,1);
         return distribution(generator);
+}
+
+static inline __m256i shift_half_left(__m256i input){
+	return _mm256_set_m128i(_mm256_extracti128_si256(input,0), zeroes128);
+}
+
+static inline __m256i shift_half_right(__m256i input){
+	return _mm256_set_m128i(zeroes128, _mm256_extracti128_si256(input,1));
 }
 
 // CONDICION INICIAL ---------------------------------------------------------------
@@ -119,9 +132,6 @@ void desestabilizacion_inicial(Manna_Array __restrict__ h)
 		h[index_a_incrementar[i]] += 1;
 	}
 }
-
-const __m128i zeroes = _mm_set_epi32(0,0,0,0);
-const __m128i ones = _mm_set_epi32(1,1,1,1);
 
 #if 0
 // DESCARGA DE ACTIVOS Y UPDATE --------------------------------------------------------
@@ -290,7 +300,7 @@ unsigned int descargar_two_loops(Manna_Array __restrict__ h_, Manna_Array __rest
 }
 #endif
 
-#define DHSZ 16
+#define DHSZ 32
 
 #ifdef DEBUG
 void printdh(Manna_Array dh){
@@ -300,7 +310,11 @@ void printdh(Manna_Array dh){
 }
 #endif
 
-unsigned int descargar(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
+#if 0
+const __m128i zeroes = _mm_set_epi32(0,0,0,0);
+const __m128i ones = _mm_set_epi32(1,1,1,1);
+
+unsigned int descargar128(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
 {
 	//~ Manna_Array __restrict__ h = (Manna_Array) __builtin_assume_aligned(h_,127);
 	//~ Manna_Array __restrict__ dh = (Manna_Array) __builtin_assume_aligned(dh_,127);
@@ -415,6 +429,126 @@ unsigned int descargar(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
 
 	return nroactivos;
 }
+#endif
+
+#define NSIMD 8
+
+
+
+unsigned int descargar(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
+{
+	unsigned int nroactivos = 0;
+	memset(dh, 0, DHSZ*(sizeof(int)));
+
+	int i = 0;
+	
+	for (i = 0; i < NSIMD; ++i) {
+		if(h[i] > 1) {
+			for (int j = 0; j < h[i]; ++j) {
+				int k = (i+2*randbool()-1+DHSZ)%DHSZ;
+				++dh[k];
+			}
+			h[i] = 0;
+		}
+		
+		if(i>1){ //actualizo salvo h[0] y h[N-1]
+			h[i-1] += dh[i-1];
+			nroactivos += (h[i-1]>1);
+		}
+	}
+	
+	#ifdef DEBUG
+	printdh(dh);
+	#endif
+	
+	__m256i left = _mm256_loadu_si256((__m256i *) &dh[i-1]); //i=NSIMD
+	__m256i right = zeroes;
+
+	for (; i < N-NSIMD; i+=NSIMD) {
+		__m256i slots = _mm256_load_si256((__m256i *) &h[i]);
+		__m256i slots_gt1 = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+		__m256i active_slots; //va a tener 0xffff en el slot si está activo
+		
+		bool activity = false;
+		while(active_slots = _mm256_and_si256(slots_gt1, _mm256_cmpgt_epi32(slots,zeroes)), _mm256_movemask_epi8(active_slots)){ //active_slots[0] or active_slots[1] or...
+			activity = true;
+			short unsigned r = rand(); //BEST
+			__m256i randomright = _mm256_set_epi32(r&1,(r>>1)&1,(r>>2)&1,(r>>3)&1,(r>>4)&1,(r>>5)&1,(r>>6)&1,(r>>7)&1); // BEST
+			__m256i randomleft = _mm256_xor_si256(randomright, ones);
+			
+			__m256i addright = _mm256_and_si256(randomright, active_slots);
+			__m256i addleft = _mm256_and_si256(randomleft, active_slots);
+
+			left = _mm256_add_epi32(left, addleft);
+			right = _mm256_add_epi32(right, addright);
+
+			slots = _mm256_sub_epi32(slots, _mm256_and_si256(active_slots, ones)); // slots - (active_slots & ones), le resto 1 a cada slot activo
+		}
+
+		//escribo en dh
+		//~ __m256i mitad = _mm256_slli_si256(right,16); //OJO, invertido
+		__m256i mitad = shift_half_left(right); //OJO, invertido
+		__m256i left_to_store = _mm256_add_epi32(left,mitad);
+
+		//~ left = _mm256_srli_si256(right,16); //OJO, invertido
+		left = shift_half_right(right); //OJO, invertido
+		right = zeroes;
+		
+		if(activity) _mm256_store_si256((__m256i *) &h[i],slots);
+		
+		//~ _mm256_storeu_si256((__m256i *) &dh[i-1],left_to_store);
+		
+		//actualizo
+		if(1 or left_to_store[0] or left_to_store[1] or left_to_store[2] or left_to_store[3]){ //if (left_to_store != 0)
+			slots = _mm256_loadu_si256((__m256i *) &h[i-1]);
+			slots = _mm256_add_epi32(slots, left_to_store);
+			_mm256_storeu_si256((__m256i *) &h[i-1], slots);
+		
+		
+			slots_gt1 = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+			slots_gt1 = _mm256_and_si256(slots_gt1,ones);
+			
+			//~ nroactivos += (slots_gt1[0]&1) + (slots_gt1[0]>>32) + (slots_gt1[1]&1) + (slots_gt1[1]>>32); //slower option
+			
+			slots_gt1 = _mm256_hadd_epi32(slots_gt1,slots_gt1); // = a0+a1, a2+a3, b0+b1, b2+b3 (pero a=b=slots_gt1)
+			slots_gt1 = _mm256_hadd_epi32(slots_gt1,slots_gt1); // = a0+a1+a2+a3, b0+b1+b2+b3, a0+a1+a2+a3, b0+b1+b2+b3
+			nroactivos += _mm256_extract_epi32(slots_gt1,0);
+		}
+	}
+
+	_mm256_storeu_si256((__m256i *) &dh[(i-1)%DHSZ],left);
+
+	#ifdef DEBUG
+	printdh(dh);
+	#endif
+
+	for (; i < N; ++i){
+		if(h[i] > 1) {
+			for (int j = 0; j < h[i]; ++j) {
+				int k = (i+2*randbool()-1)%DHSZ;
+				++dh[k];
+			}
+			h[i] = 0;
+		}
+		//actualizo
+		h[i-1] += dh[(i-1)%DHSZ];
+        nroactivos += (h[i-1]>1);
+	}
+	
+	//actualizo N-1
+    h[N-1] += dh[(N-1)%DHSZ];
+    nroactivos += (h[N-1]>1);
+    
+	//actualizo 0
+    h[0] += dh[0];
+    nroactivos += (h[0]>1);
+
+	#ifdef DEBUG
+	printdh(dh);
+	#endif
+
+	return nroactivos;
+}
 
 //===================================================================
 // Lo compilo asi: g++ tiny_manna.cpp -std=c++0x
@@ -422,13 +556,20 @@ int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 
 	randinit();
-	
-/*
+	/*
 int a[8]={1,2,3,4,5,6,7,8};
-__m128i aver = _mm_loadu_si128((__m128i *)a);
-cout<<printear(aver)<<endl;
-aver = _mm_srli_si128(aver,8);
-cout<<printear(aver)<<endl;
+__m256i aver = _mm256_loadu_si256((__m256i *)a);
+
+cout<<printear256(aver)<<endl;
+
+__m256i aver1 = _mm256_set_m128i(_mm256_extracti128_si256(aver,0), zeroes128);
+cout<<printear256(aver1)<<endl;
+aver1 = _mm256_set_m128i(_mm256_extracti128_si256(aver,1), zeroes128);
+cout<<printear256(aver1)<<endl;
+aver1 = _mm256_set_m128i(zeroes128, _mm256_extracti128_si256(aver,0));
+cout<<printear256(aver1)<<endl;
+aver1 = _mm256_set_m128i(zeroes128, _mm256_extracti128_si256(aver,1));
+cout<<printear256(aver1)<<endl;
 return 0;
 */
 	#ifdef DEBUG
