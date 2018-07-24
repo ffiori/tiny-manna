1c8afc3a35f543685464f2aabe02fdc4e43871b6
Fefo <fefo_f93@hotmail.com>
2018-07-24 14:20:03 -0300

    openmp first

---
+++ openmp/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=fiori
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:0
+#SBATCH --time 1-0:00
+
+. /etc/profile
+. /opt/ipsxe/2018u2/vtune_amplifier/amplxe-vars.sh
+
+export LC_ALL=C.UTF-8
+
+srun sh -c 'sh test-all.sh'
+
+# "sbatch archivo" lo manda a correr
+# "scontrol show job NNNN" te muestra data
+# y te sale un archivo tipo en el home slurm-NNN.out con la salida de los comandos
