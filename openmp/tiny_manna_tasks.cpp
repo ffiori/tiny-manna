8b7dd6f4fb6dcf24e6841a7d5470110c9d226a03
Fefo <fefo_f93@hotmail.com>
2018-07-31 16:21:40 -0300

    OpenMP tasks works, faster than simd

--- openmp/tiny_manna_tasks.cpp
+++ openmp/tiny_manna_tasks.cpp
@@ -40,6 +40,11 @@ Notar que si la densidad de granitos, [Suma_i h[i]/N] es muy baja, la actividad 
 // number of temporal steps
 #define NSTEPS 10000
 
+#define DHSZ 32
+#define NSIMD 8
+#define NTASKS 32
+#define STEP ((N-2*NSIMD)/NTASKS - ((N-2*NSIMD)/NTASKS)%8 + 8) //para que sea aligned
+
 using namespace std;
 
 typedef double REAL;
@@ -66,7 +71,7 @@ static inline bool randbool() { //return 1;
 	return distribution(generator);
 }
 
-static inline unsigned char randchar() { //return 255;
+static inline unsigned char randchar() { //return 0;
 	uniform_int_distribution<unsigned char> distribution(0,255);
 	return distribution(generator);
 }
@@ -135,6 +140,8 @@ void desestabilizacion_inicial(Manna_Array __restrict__ h)
 		if (h[i] == 1) {
 			h[i] = 0;
 			int j=(i+2*randbool()-1+N)%N; // izquierda o derecha
+			//~ int j=(i+2*((i%3)%2)-1+N)%N; // izquierda o derecha
+			
 			index_a_incrementar.push_back(j);
 		}
 	}
@@ -143,33 +150,26 @@ void desestabilizacion_inicial(Manna_Array __restrict__ h)
 	}
 }
 
-#define DHSZ 32
-
-#ifdef DEBUG
 void printdh(Manna_Array dh){
 	cout<<"dh[]: ";
 	for(int i=0; i<DHSZ; i++) cout<<dh[i]<<" ";
 	cout<<endl;
 }
-#endif
-
-#define NSIMD 8
 
 const __m256i MASK[256] = {
 _mm256_blend_epi32(ones,zeroes, 0 ), _mm256_blend_epi32(ones,zeroes, 1 ), _mm256_blend_epi32(ones,zeroes, 2 ), _mm256_blend_epi32(ones,zeroes, 3 ), _mm256_blend_epi32(ones,zeroes, 4 ), _mm256_blend_epi32(ones,zeroes, 5 ), _mm256_blend_epi32(ones,zeroes, 6 ), _mm256_blend_epi32(ones,zeroes, 7 ), _mm256_blend_epi32(ones,zeroes, 8 ), _mm256_blend_epi32(ones,zeroes, 9 ), _mm256_blend_epi32(ones,zeroes, 10 ), _mm256_blend_epi32(ones,zeroes, 11 ), _mm256_blend_epi32(ones,zeroes, 12 ), _mm256_blend_epi32(ones,zeroes, 13 ), _mm256_blend_epi32(ones,zeroes, 14 ), _mm256_blend_epi32(ones,zeroes, 15 ), _mm256_blend_epi32(ones,zeroes, 16 ), _mm256_blend_epi32(ones,zeroes, 17 ), _mm256_blend_epi32(ones,zeroes, 18 ), _mm256_blend_epi32(ones,zeroes, 19 ), _mm256_blend_epi32(ones,zeroes, 20 ), _mm256_blend_epi32(ones,zeroes, 21 ), _mm256_blend_epi32(ones,zeroes, 22 ), _mm256_blend_epi32(ones,zeroes, 23 ), _mm256_blend_epi32(ones,zeroes, 24 ), _mm256_blend_epi32(ones,zeroes, 25 ), _mm256_blend_epi32(ones,zeroes, 26 ), _mm256_blend_epi32(ones,zeroes, 27 ), _mm256_blend_epi32(ones,zeroes, 28 ), _mm256_blend_epi32(ones,zeroes, 29 ), _mm256_blend_epi32(ones,zeroes, 30 ), _mm256_blend_epi32(ones,zeroes, 31 ), _mm256_blend_epi32(ones,zeroes, 32 ), _mm256_blend_epi32(ones,zeroes, 33 ), _mm256_blend_epi32(ones,zeroes, 34 ), _mm256_blend_epi32(ones,zeroes, 35 ), _mm256_blend_epi32(ones,zeroes, 36 ), _mm256_blend_epi32(ones,zeroes, 37 ), _mm256_blend_epi32(ones,zeroes, 38 ), _mm256_blend_epi32(ones,zeroes, 39 ), _mm256_blend_epi32(ones,zeroes, 40 ), _mm256_blend_epi32(ones,zeroes, 41 ), _mm256_blend_epi32(ones,zeroes, 42 ), _mm256_blend_epi32(ones,zeroes, 43 ), _mm256_blend_epi32(ones,zeroes, 44 ), _mm256_blend_epi32(ones,zeroes, 45 ), _mm256_blend_epi32(ones,zeroes, 46 ), _mm256_blend_epi32(ones,zeroes, 47 ), _mm256_blend_epi32(ones,zeroes, 48 ), _mm256_blend_epi32(ones,zeroes, 49 ), _mm256_blend_epi32(ones,zeroes, 50 ), _mm256_blend_epi32(ones,zeroes, 51 ), _mm256_blend_epi32(ones,zeroes, 52 ), _mm256_blend_epi32(ones,zeroes, 53 ), _mm256_blend_epi32(ones,zeroes, 54 ), _mm256_blend_epi32(ones,zeroes, 55 ), _mm256_blend_epi32(ones,zeroes, 56 ), _mm256_blend_epi32(ones,zeroes, 57 ), _mm256_blend_epi32(ones,zeroes, 58 ), _mm256_blend_epi32(ones,zeroes, 59 ), _mm256_blend_epi32(ones,zeroes, 60 ), _mm256_blend_epi32(ones,zeroes, 61 ), _mm256_blend_epi32(ones,zeroes, 62 ), _mm256_blend_epi32(ones,zeroes, 63 ), _mm256_blend_epi32(ones,zeroes, 64 ), _mm256_blend_epi32(ones,zeroes, 65 ), _mm256_blend_epi32(ones,zeroes, 66 ), _mm256_blend_epi32(ones,zeroes, 67 ), _mm256_blend_epi32(ones,zeroes, 68 ), _mm256_blend_epi32(ones,zeroes, 69 ), _mm256_blend_epi32(ones,zeroes, 70 ), _mm256_blend_epi32(ones,zeroes, 71 ), _mm256_blend_epi32(ones,zeroes, 72 ), _mm256_blend_epi32(ones,zeroes, 73 ), _mm256_blend_epi32(ones,zeroes, 74 ), _mm256_blend_epi32(ones,zeroes, 75 ), _mm256_blend_epi32(ones,zeroes, 76 ), _mm256_blend_epi32(ones,zeroes, 77 ), _mm256_blend_epi32(ones,zeroes, 78 ), _mm256_blend_epi32(ones,zeroes, 79 ), _mm256_blend_epi32(ones,zeroes, 80 ), _mm256_blend_epi32(ones,zeroes, 81 ), _mm256_blend_epi32(ones,zeroes, 82 ), _mm256_blend_epi32(ones,zeroes, 83 ), _mm256_blend_epi32(ones,zeroes, 84 ), _mm256_blend_epi32(ones,zeroes, 85 ), _mm256_blend_epi32(ones,zeroes, 86 ), _mm256_blend_epi32(ones,zeroes, 87 ), _mm256_blend_epi32(ones,zeroes, 88 ), _mm256_blend_epi32(ones,zeroes, 89 ), _mm256_blend_epi32(ones,zeroes, 90 ), _mm256_blend_epi32(ones,zeroes, 91 ), _mm256_blend_epi32(ones,zeroes, 92 ), _mm256_blend_epi32(ones,zeroes, 93 ), _mm256_blend_epi32(ones,zeroes, 94 ), _mm256_blend_epi32(ones,zeroes, 95 ), _mm256_blend_epi32(ones,zeroes, 96 ), _mm256_blend_epi32(ones,zeroes, 97 ), _mm256_blend_epi32(ones,zeroes, 98 ), _mm256_blend_epi32(ones,zeroes, 99 ), _mm256_blend_epi32(ones,zeroes, 100 ), _mm256_blend_epi32(ones,zeroes, 101 ), _mm256_blend_epi32(ones,zeroes, 102 ), _mm256_blend_epi32(ones,zeroes, 103 ), _mm256_blend_epi32(ones,zeroes, 104 ), _mm256_blend_epi32(ones,zeroes, 105 ), _mm256_blend_epi32(ones,zeroes, 106 ), _mm256_blend_epi32(ones,zeroes, 107 ), _mm256_blend_epi32(ones,zeroes, 108 ), _mm256_blend_epi32(ones,zeroes, 109 ), _mm256_blend_epi32(ones,zeroes, 110 ), _mm256_blend_epi32(ones,zeroes, 111 ), _mm256_blend_epi32(ones,zeroes, 112 ), _mm256_blend_epi32(ones,zeroes, 113 ), _mm256_blend_epi32(ones,zeroes, 114 ), _mm256_blend_epi32(ones,zeroes, 115 ), _mm256_blend_epi32(ones,zeroes, 116 ), _mm256_blend_epi32(ones,zeroes, 117 ), _mm256_blend_epi32(ones,zeroes, 118 ), _mm256_blend_epi32(ones,zeroes, 119 ), _mm256_blend_epi32(ones,zeroes, 120 ), _mm256_blend_epi32(ones,zeroes, 121 ), _mm256_blend_epi32(ones,zeroes, 122 ), _mm256_blend_epi32(ones,zeroes, 123 ), _mm256_blend_epi32(ones,zeroes, 124 ), _mm256_blend_epi32(ones,zeroes, 125 ), _mm256_blend_epi32(ones,zeroes, 126 ), _mm256_blend_epi32(ones,zeroes, 127 ), _mm256_blend_epi32(ones,zeroes, 128 ), _mm256_blend_epi32(ones,zeroes, 129 ), _mm256_blend_epi32(ones,zeroes, 130 ), _mm256_blend_epi32(ones,zeroes, 131 ), _mm256_blend_epi32(ones,zeroes, 132 ), _mm256_blend_epi32(ones,zeroes, 133 ), _mm256_blend_epi32(ones,zeroes, 134 ), _mm256_blend_epi32(ones,zeroes, 135 ), _mm256_blend_epi32(ones,zeroes, 136 ), _mm256_blend_epi32(ones,zeroes, 137 ), _mm256_blend_epi32(ones,zeroes, 138 ), _mm256_blend_epi32(ones,zeroes, 139 ), _mm256_blend_epi32(ones,zeroes, 140 ), _mm256_blend_epi32(ones,zeroes, 141 ), _mm256_blend_epi32(ones,zeroes, 142 ), _mm256_blend_epi32(ones,zeroes, 143 ), _mm256_blend_epi32(ones,zeroes, 144 ), _mm256_blend_epi32(ones,zeroes, 145 ), _mm256_blend_epi32(ones,zeroes, 146 ), _mm256_blend_epi32(ones,zeroes, 147 ), _mm256_blend_epi32(ones,zeroes, 148 ), _mm256_blend_epi32(ones,zeroes, 149 ), _mm256_blend_epi32(ones,zeroes, 150 ), _mm256_blend_epi32(ones,zeroes, 151 ), _mm256_blend_epi32(ones,zeroes, 152 ), _mm256_blend_epi32(ones,zeroes, 153 ), _mm256_blend_epi32(ones,zeroes, 154 ), _mm256_blend_epi32(ones,zeroes, 155 ), _mm256_blend_epi32(ones,zeroes, 156 ), _mm256_blend_epi32(ones,zeroes, 157 ), _mm256_blend_epi32(ones,zeroes, 158 ), _mm256_blend_epi32(ones,zeroes, 159 ), _mm256_blend_epi32(ones,zeroes, 160 ), _mm256_blend_epi32(ones,zeroes, 161 ), _mm256_blend_epi32(ones,zeroes, 162 ), _mm256_blend_epi32(ones,zeroes, 163 ), _mm256_blend_epi32(ones,zeroes, 164 ), _mm256_blend_epi32(ones,zeroes, 165 ), _mm256_blend_epi32(ones,zeroes, 166 ), _mm256_blend_epi32(ones,zeroes, 167 ), _mm256_blend_epi32(ones,zeroes, 168 ), _mm256_blend_epi32(ones,zeroes, 169 ), _mm256_blend_epi32(ones,zeroes, 170 ), _mm256_blend_epi32(ones,zeroes, 171 ), _mm256_blend_epi32(ones,zeroes, 172 ), _mm256_blend_epi32(ones,zeroes, 173 ), _mm256_blend_epi32(ones,zeroes, 174 ), _mm256_blend_epi32(ones,zeroes, 175 ), _mm256_blend_epi32(ones,zeroes, 176 ), _mm256_blend_epi32(ones,zeroes, 177 ), _mm256_blend_epi32(ones,zeroes, 178 ), _mm256_blend_epi32(ones,zeroes, 179 ), _mm256_blend_epi32(ones,zeroes, 180 ), _mm256_blend_epi32(ones,zeroes, 181 ), _mm256_blend_epi32(ones,zeroes, 182 ), _mm256_blend_epi32(ones,zeroes, 183 ), _mm256_blend_epi32(ones,zeroes, 184 ), _mm256_blend_epi32(ones,zeroes, 185 ), _mm256_blend_epi32(ones,zeroes, 186 ), _mm256_blend_epi32(ones,zeroes, 187 ), _mm256_blend_epi32(ones,zeroes, 188 ), _mm256_blend_epi32(ones,zeroes, 189 ), _mm256_blend_epi32(ones,zeroes, 190 ), _mm256_blend_epi32(ones,zeroes, 191 ), _mm256_blend_epi32(ones,zeroes, 192 ), _mm256_blend_epi32(ones,zeroes, 193 ), _mm256_blend_epi32(ones,zeroes, 194 ), _mm256_blend_epi32(ones,zeroes, 195 ), _mm256_blend_epi32(ones,zeroes, 196 ), _mm256_blend_epi32(ones,zeroes, 197 ), _mm256_blend_epi32(ones,zeroes, 198 ), _mm256_blend_epi32(ones,zeroes, 199 ), _mm256_blend_epi32(ones,zeroes, 200 ), _mm256_blend_epi32(ones,zeroes, 201 ), _mm256_blend_epi32(ones,zeroes, 202 ), _mm256_blend_epi32(ones,zeroes, 203 ), _mm256_blend_epi32(ones,zeroes, 204 ), _mm256_blend_epi32(ones,zeroes, 205 ), _mm256_blend_epi32(ones,zeroes, 206 ), _mm256_blend_epi32(ones,zeroes, 207 ), _mm256_blend_epi32(ones,zeroes, 208 ), _mm256_blend_epi32(ones,zeroes, 209 ), _mm256_blend_epi32(ones,zeroes, 210 ), _mm256_blend_epi32(ones,zeroes, 211 ), _mm256_blend_epi32(ones,zeroes, 212 ), _mm256_blend_epi32(ones,zeroes, 213 ), _mm256_blend_epi32(ones,zeroes, 214 ), _mm256_blend_epi32(ones,zeroes, 215 ), _mm256_blend_epi32(ones,zeroes, 216 ), _mm256_blend_epi32(ones,zeroes, 217 ), _mm256_blend_epi32(ones,zeroes, 218 ), _mm256_blend_epi32(ones,zeroes, 219 ), _mm256_blend_epi32(ones,zeroes, 220 ), _mm256_blend_epi32(ones,zeroes, 221 ), _mm256_blend_epi32(ones,zeroes, 222 ), _mm256_blend_epi32(ones,zeroes, 223 ), _mm256_blend_epi32(ones,zeroes, 224 ), _mm256_blend_epi32(ones,zeroes, 225 ), _mm256_blend_epi32(ones,zeroes, 226 ), _mm256_blend_epi32(ones,zeroes, 227 ), _mm256_blend_epi32(ones,zeroes, 228 ), _mm256_blend_epi32(ones,zeroes, 229 ), _mm256_blend_epi32(ones,zeroes, 230 ), _mm256_blend_epi32(ones,zeroes, 231 ), _mm256_blend_epi32(ones,zeroes, 232 ), _mm256_blend_epi32(ones,zeroes, 233 ), _mm256_blend_epi32(ones,zeroes, 234 ), _mm256_blend_epi32(ones,zeroes, 235 ), _mm256_blend_epi32(ones,zeroes, 236 ), _mm256_blend_epi32(ones,zeroes, 237 ), _mm256_blend_epi32(ones,zeroes, 238 ), _mm256_blend_epi32(ones,zeroes, 239 ), _mm256_blend_epi32(ones,zeroes, 240 ), _mm256_blend_epi32(ones,zeroes, 241 ), _mm256_blend_epi32(ones,zeroes, 242 ), _mm256_blend_epi32(ones,zeroes, 243 ), _mm256_blend_epi32(ones,zeroes, 244 ), _mm256_blend_epi32(ones,zeroes, 245 ), _mm256_blend_epi32(ones,zeroes, 246 ), _mm256_blend_epi32(ones,zeroes, 247 ), _mm256_blend_epi32(ones,zeroes, 248 ), _mm256_blend_epi32(ones,zeroes, 249 ), _mm256_blend_epi32(ones,zeroes, 250 ), _mm256_blend_epi32(ones,zeroes, 251 ), _mm256_blend_epi32(ones,zeroes, 252 ), _mm256_blend_epi32(ones,zeroes, 253 ), _mm256_blend_epi32(ones,zeroes, 254 ), _mm256_blend_epi32(ones,zeroes, 255 )
-};
+}; //MASK[0]={1,1,1,...}, MASK[255]={0,0,0,...}
 
-#define NTASKS 8
-#define STEP ((N-2*NSIMD)/NTASKS - ((N-2*NSIMD)/NTASKS)%8 + 8) //para que sea aligned
-__m256i left_border[NTASKS];
+__m256i left_border[NTASKS],right_border[NTASKS];
 
-//descarga slots [from,to), actualiza h (salvo borde derecho) y devuelve cantidad de slots activos en result.
-void process(int from, int to, int tasknum, Manna_Array __restrict__ h, unsigned int *result)
+//descarga slots [from,to), actualiza h (salvo borde derecho h[to-1] y h[to], que guarda el dh en left_border[tasknum]), y devuelve cantidad de slots activos en result.
+static inline void process(int from, int to, int tasknum, Manna_Array __restrict__ h, unsigned int *result)
 {
 	#ifdef DEBUG
 	cout<<"process from "<<from<<" to "<<to<<". tasknum "<<tasknum<<endl;
 	#endif
 	
+	left_border[tasknum] = zeroes;
 	unsigned int nroactivos = 0;
 	__m256i left = zeroes;
 	__m256i right = zeroes;
@@ -206,16 +206,21 @@ void process(int from, int to, int tasknum, Manna_Array __restrict__ h, unsigned
 		
 		//actualizo
 		if(!_mm256_testz_si256(left_to_store,left_to_store)){ //if (left_to_store != 0)
-			slots = _mm256_loadu_si256((__m256i *) &h[i-1]);
-			slots = _mm256_add_epi32(slots, left_to_store);
-			_mm256_storeu_si256((__m256i *) &h[i-1], slots);
-		
-			__m256i tmp = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
-			nroactivos += __builtin_popcount(_mm256_movemask_epi8(tmp))/4;
+			if(i==from){ //si es la primera iteración guardo el borde para procesarlo después (se puede sacar afuera para optimizar)
+				left_border[tasknum] = left_to_store;
+			}
+			else{
+				slots = _mm256_loadu_si256((__m256i *) &h[i-1]);
+				slots = _mm256_add_epi32(slots, left_to_store);
+				_mm256_storeu_si256((__m256i *) &h[i-1], slots);
+			
+				__m256i tmp = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+				nroactivos += __builtin_popcount(_mm256_movemask_epi8(tmp))/4;
+			}
 		}
 	}
 	
-	left_border[tasknum] = left;
+	right_border[tasknum] = left;
 	*result = nroactivos;
 }
 
@@ -233,52 +238,35 @@ unsigned int descargar(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
 			h[i] = 0;
 		}
 		
-		if(i>1){ //actualizo salvo h[0] y h[N-1]
+		if(i>1){ //actualizo salvo h[0] y h[N-1], y h[NSIMD-1]...
 			h[i-1] += dh[i-1];
 			nroactivos += (h[i-1]>1);
 		}
 	}
 	
-	unsigned int activos[NTASKS];
+	unsigned int activos[NTASKS]={0};
 	
 	#pragma omp parallel
-    #pragma omp single nowait
+	{
+    #pragma omp single //nowait
     {
-	//lanzo una task por cada pedazo de for
+	//lanzo una task por cada pedazo de for. Hace como un #pragma omp parallel for schedule(dynamic,STEP)
 	for (int i=NSIMD; i < N-NSIMD; i += STEP) {
 		#ifdef DEBUG
 		cout<<"from i "<<i<<" to "<<i+STEP<<" STEP: "<<STEP<<". NTASKS "<<NTASKS<<" current task "<<(i-NSIMD)/STEP<<endl;
 		#endif
 		
-		int tasknum = (i-NSIMD)/STEP;
-		
-		#pragma omp task shared(activos,nroactivos)
+		#pragma omp task firstprivate(i) default(none) shared(activos,h)
 		{
+		int tasknum = (i-NSIMD)/STEP;
 		process(i,min(N-NSIMD,i+STEP),tasknum,h,&activos[tasknum]);
 		}
 	}
 	
-	#pragma omp taskwait 
+	#pragma omp taskwait
+	}
 	}
 	
-	//unir left_borders a h
-	for(int t=0; t<NTASKS; ++t){
-		int slot = NSIMD + (t+1)*STEP - 1;
-		
-		#ifdef DEBUG
-		cout<<"Slot "<<slot<<" task: "<<t<<". activos "<<activos[t]<<endl;
-		#endif
-		
-		nroactivos -= (h[slot]>1);
-		nroactivos -= (h[slot+1]>1);
-		h[slot] += _mm256_extract_epi32(left_border[t],0);
-		h[slot+1] += _mm256_extract_epi32(left_border[t],1);
-		nroactivos += (h[slot]>1) + (h[slot+1]>1);
-		
-		nroactivos += activos[t]; //acumulados de la task
-	}
-
-
 	for (int i=N-NSIMD; i < N; ++i){
 		if(h[i] > 1) {
 			for (int j = 0; j < h[i]; ++j) {
@@ -292,6 +280,75 @@ unsigned int descargar(Manna_Array __restrict__ h, Manna_Array __restrict__ dh)
         nroactivos += (h[i-1]>1);
 	}
 	
+	//update borders
+	for(int t=0; t<NTASKS; ++t){ //TODO se puede optimizar sumando los left y right
+		int slot = NSIMD + t*STEP;
+		
+		//left_border
+		__m256i slots = _mm256_loadu_si256((__m256i *) &h[slot-1]);
+		slots = _mm256_add_epi32(slots, left_border[t]);
+		_mm256_storeu_si256((__m256i *) &h[slot-1], slots);
+		
+		__m256i tmp = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+		nroactivos += __builtin_popcount(_mm256_movemask_epi8(tmp))/4;
+
+		//right_border
+		slot = min(slot+STEP, N-NSIMD);
+		if(slot==N-NSIMD){ //already counted in nroactivos
+			nroactivos -= (h[slot]>1);
+			nroactivos -= (h[slot-1]>1);
+		}
+		h[slot] += _mm256_extract_epi32(right_border[t],0);
+		h[slot-1] += _mm256_extract_epi32(right_border[t],1);
+		if(slot==N-NSIMD){ //counted in left_border above, except for last task
+			nroactivos += (h[slot]>1);
+			nroactivos += (h[slot-1]>1);
+		}
+		
+		nroactivos += activos[t]; //acumulados de la task
+	}
+
+
+	//~ //update borders///////////////////////////////////////////////////
+	//~ //first task
+	//~ int slot = NSIMD;
+	//~ __m256i slots = _mm256_loadu_si256((__m256i *) &h[slot-1]);
+	//~ slots = _mm256_add_epi32(slots, left_border[0]);
+	//~ _mm256_storeu_si256((__m256i *) &h[slot-1], slots);
+	//~ __m256i tmp = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+	//~ nroactivos += __builtin_popcount(_mm256_movemask_epi8(tmp))/4;
+
+	//~ //middle tasks
+	//~ for(int t=0; t<NTASKS-1; ++t){ //TODO se puede optimizar sumando los left y right
+		//~ slot = min(slot+STEP, N-NSIMD); //check if slot can be >N-NSIMD before NTASKS tasks
+		//~ __m256i slots = _mm256_loadu_si256((__m256i *) &h[slot-1]);
+		//~ slots = _mm256_add_epi32(slots, left_border[t+1]);
+		//~ slots = _mm256_add_epi32(slots, right_border[t]);
+		//~ _mm256_storeu_si256((__m256i *) &h[slot-1], slots);
+		
+		//~ __m256i tmp = _mm256_cmpgt_epi32(slots,ones); //slots greater than 1
+		//~ nroactivos += __builtin_popcount(_mm256_movemask_epi8(tmp))/4;
+
+		//~ nroactivos += activos[t]; //acumulados de la task
+	//~ }
+
+	//~ //last task
+	//~ slot = N-NSIMD;
+	
+	//~ //already counted in nroactivos
+	//~ nroactivos -= (h[slot]>1);
+	//~ nroactivos -= (h[slot-1]>1);
+	
+	//~ h[slot] += _mm256_extract_epi32(right_border[NTASKS-1],0);
+	//~ h[slot-1] += _mm256_extract_epi32(right_border[NTASKS-1],1);
+	
+	//~ //counted in left_border above, except for last task
+	//~ nroactivos += (h[slot]>1);
+	//~ nroactivos += (h[slot-1]>1);
+
+	//~ nroactivos += activos[NTASKS-1]; //acumulados de la task
+//~ /////////////////////////////////////
+
 	//actualizo N-1
     h[N-1] += dh[(N-1)%DHSZ];
     nroactivos += (h[N-1]>1);
@@ -356,10 +413,10 @@ int main(){
 }
 
 /*
- * Ejecutar con zx81$ OMP_NUM_THREADS=4 taskset -c 0-5 numactl --interleave=all perf_4.17 stat -d -r 1 ./tiny_manna_tasks
+ * Ejecutar con zx81$ OMP_NUM_THREADS=6 taskset -c 0-5 numactl --interleave=all perf_4.17 stat -d -r 1 ./tiny_manna_tasks
  * TODO:
- *   Chequear si anda!!!
- *   Pulir, se pueden optimizar un par de fors capaz.
- *   Buscar valores óptimos para NTASKS.
+ *   Chequear si anda!!! -> furula
+ *   Pulir, se pueden optimizar un par de fors capaz. -> el update borders no mejora
+ *   Buscar valores óptimos para NTASKS. -> 32 o 64. Capaz convenga hacerlo depender de N.
  *   Tener en cuenta que la versión final hay que testear con DENSITY 0.8924
  */ 
